969636a42b544708888a0fc2f0fc49cf81f3aa94
Shane-Stevenson <shanestevenson04@gmail.com>
2023-05-29 13:49:47 -0500

    finish voxelization, start AI

--- voxelize.py
+++ voxelize.py
@@ -8,24 +8,57 @@ def voxelize(points : list, voxel_size : int):
     # Add the points, colors and normals as Vectors
     pcd.points = o3d.utility.Vector3dVector(points)
     colors = []
-    normals = []
     for i in points:
         colors.append([.5, .5, .5])
-        normals.append([.5, .5, .5])
     pcd.colors = o3d.utility.Vector3dVector(colors)
-    pcd.normals = o3d.utility.Vector3dVector(normals)
-    #Create a voxel grid from the point cloud with a voxel_size of 0.01
-    voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=voxel_size)
+    #Create a voxel grid from the point cloud with a voxel_size of voxel_size and with a minimum bound of 0 and maximum bound of 10
+    mins = np.array([0, 0, 0])      #min and max could be changed to function arguments for more manuverability
+    maxs = np.array([10, 10, 10])
+    voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(pcd, voxel_size, mins, maxs)
+
+    for v in voxel_grid.get_voxels():
+        print(v.grid_index)
+
+    #make new pointcloud and then a new voxelGrid
+    fullPoints = []
+    fullColors = []
+    xyz = []
+    for x in range(int(maxs[0]/voxel_size)):
+        for y in range(int(maxs[1]/voxel_size)):
+            for z in range(int(maxs[2]/voxel_size)):
+                xyz.append([x*voxel_size, y*voxel_size, z*voxel_size])
+    
+    #If the voxel exists in the one we created from the original point cloud, color it and add it to the new voxel grid
+    #If the voxel does not exist in the original voxel grid, color it black and add it to the new voxel grid
+    #The colors is how the AI will distinguish voxels that represent points and voxels that represent empty space
+    bools = voxel_grid.check_if_included(o3d.utility.Vector3dVector(xyz))
+    for i in range(len(bools)):
+        if(bools[i]):
+            fullPoints.append(xyz[i])
+            fullColors.append([.5, .5, .5])
+        else:
+            fullPoints.append(xyz[i])
+            fullColors.append([0, 0, 0])
+
+    fullpcd = o3d.geometry.PointCloud()
+    fullpcd.points = o3d.utility.Vector3dVector(fullPoints)
+    fullpcd.colors = o3d.utility.Vector3dVector(fullColors)
+    full_voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(fullpcd, voxel_size, mins, maxs)
+
+    # for v in full_voxel_grid.get_voxels():
+    #     print(v.grid_index)
+    print('min: ', full_voxel_grid.get_min_bound())
+    print('max: ', full_voxel_grid.get_max_bound())
 
     # Initialize a visualizer object
     vis = o3d.visualization.Visualizer()
     # Create a window, name it and scale it
-    vis.create_window(window_name='Bunny Visualize', width=800, height=600)
+    vis.create_window(window_name='Open 3D visualizer', width=800, height=600)
 
     # Add the voxel grid to the visualizer
-    vis.add_geometry(voxel_grid)
+    vis.add_geometry(full_voxel_grid)
 
-    # We run the visualizater
+    # run the visualizater
     vis.run()
     # Once the visualizer is closed destroy the window and clean up
     vis.destroy_window()
