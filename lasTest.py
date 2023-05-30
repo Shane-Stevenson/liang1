29d7ba3f45c813be944386a6a1fa32b6b7d5baa3
Shane-Stevenson <shanestevenson04@gmail.com>
2023-05-30 10:24:15 -0500

    cleaning, more has to be done

--- lasTest.py
+++ lasTest.py
@@ -12,7 +12,7 @@ def iterate_over_las(las_file : str, square_length : int):
     print(las)
 
     #Recording the data in a numpy array
-    point_data = np.stack([las.x, las.y, las.z], axis = 0).transpose((1,0))
+    point_data = np.stack([las.x, las.y, las.z, las.classification], axis = 0).transpose((1,0))
     print(point_data)
 
 
@@ -23,12 +23,19 @@ def iterate_over_las(las_file : str, square_length : int):
         
     print('looping...')
 
-    #initialize a 222x262 2D array
+    #initialize a 222x262 2D array for the points
     boxes = []
     for i in range(222):
         boxes.append([])
         for j in range(262):
             boxes[i].append([])
+
+    #initialize a 222x262 2D array for the colors
+    colors = []
+    for i in range(222):
+        colors.append([])
+        for j in range(262):
+            colors[i].append([])
 
     """
     The data is 1486 units long and 1758 units high. We place each point in its corresponding x-Box by dividing the difference of the 
@@ -38,26 +45,59 @@ def iterate_over_las(las_file : str, square_length : int):
     for i in point_data:
         xIndex = math.floor((i[0] - XMIN)/square_length)
         yIndex = math.floor((i[1] - YMIN)/square_length)
-        boxes[xIndex][yIndex].append([i[0], i[1], i[2]])
 
-    #Initialize x,y,z lists
+        #Add the points and colors to boxes
+        match i[3]:
+            case 1:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.416, 0.424, 0.471])
+            case 2:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.541, 0.357, 0.173])
+            case 3:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.329, 0.62, 0.8])
+            case 4: 
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.137, 0.922, 0.216])
+            case 5:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.29, 0.69, 0.333])
+            case 6:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.922, 0.149, 0.149])
+            case 7:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.541, 0.145, 0.145])
+            case 8: 
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.165, 0.843, 0.878])
+            case 9:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.898, 0.941, 0.192])
+            case 10:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.337, 0.251, 0.749])
+            case 11:
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.282, 0.286, 0.322])
+            case 12: 
+                boxes[xIndex][yIndex].append([i[0], i[1], i[2], 0.949, 0.314, 0.651])
+
+    #Initialize x,y,z, and colors lists
     x_data = []
     y_data = []
     z_data = []
+    colors = []
     for i in boxes:
         for j in i:
             #Clear the lists when moving to a new box
             x_data.clear()
             y_data.clear()
             z_data.clear()
+            colors.clear()
+            green = False
             for k in j:
                 #fill the lists with all the values in the current box
                 x_data.append(k[0])
                 y_data.append(k[1])
                 z_data.append(k[2])
+                colors.append([k[3],k[4], k[5]])
+                if k[3] == 0.137 or k[3] == 0.29:
+                    green = True
             
             #If there is no x or y data, or if the boxe's x or y length is short it means we are on an edge piece and we should skip
-            if(len(x_data) == 0 or len(y_data) == 0 or max(x_data) - min(x_data) < 8 or max(y_data) - min(y_data) < 8):
+            if(len(x_data) == 0 or len(y_data) == 0 or max(x_data) - min(x_data) < 8 or max(y_data) - min(y_data) < 8 
+               or max(z_data) - min(z_data) < 8):
                 continue
             
             #Print the data of the current box
@@ -66,18 +106,19 @@ def iterate_over_las(las_file : str, square_length : int):
             print('ymin: ', min(y_data), 'ymax: ', max(y_data), 'diff: ', max(y_data) - min(y_data))
             print('zmin: ', min(z_data), 'zmax: ', max(z_data), 'diff: ', max(z_data) - min(z_data))
 
-            #draw the points using matplotlib
-            fig = plt.figure(figsize=(5, 5))
-            ax = fig.add_subplot(111, projection="3d")
-            ax.scatter(x_data, y_data, z_data)
-            ax.set_axis_off()
-            plt.show()
+            if green:
+                #draw the points using matplotlib
+                fig = plt.figure(figsize=(5, 5))
+                ax = fig.add_subplot(111, projection="3d")
+                ax.scatter(x_data, y_data, z_data)
+                ax.set_axis_off()
+                plt.show()
 
-            #voxelize
-            points = []
-            xmin = min(x_data)
-            ymin = min(y_data)
-            zmin = min(z_data)
-            for i in range(len(x_data)):
-                points.append([x_data[i] - xmin, y_data[i] - ymin, z_data[i] - zmin])
-            voxelize(points, .25)
+                #voxelize
+                points = []
+                xmin = min(x_data)
+                ymin = min(y_data)
+                zmin = min(z_data)
+                for i in range(len(x_data)):
+                    points.append([x_data[i] - xmin, y_data[i] - ymin, z_data[i] - zmin])
+                voxelize(points, colors, .25)
--- voxelize.py
+++ voxelize.py
@@ -1,41 +1,54 @@
 import open3d as o3d
 import numpy as np
 
-def voxelize(points : list, voxel_size : int):
+def voxelize(points : list, colors : list, voxel_size : int):
     # Initialize a point cloud object
     pcd = o3d.geometry.PointCloud()
 
     # Add the points, colors and normals as Vectors
     pcd.points = o3d.utility.Vector3dVector(points)
-    colors = []
-    for i in points:
-        colors.append([.5, .5, .5])
     pcd.colors = o3d.utility.Vector3dVector(colors)
     #Create a voxel grid from the point cloud with a voxel_size of voxel_size and with a minimum bound of 0 and maximum bound of 10
     mins = np.array([0, 0, 0])      #min and max could be changed to function arguments for more manuverability
     maxs = np.array([10, 10, 10])
     voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(pcd, voxel_size, mins, maxs)
+    print(voxel_grid.get_max_bound())
 
-    for v in voxel_grid.get_voxels():
-        print(v.grid_index)
+    # Initialize a visualizer object
+    vis = o3d.visualization.Visualizer()
+    # Create a window, name it and scale it
+    vis.create_window(window_name='Open 3D visualizer', width=800, height=600)
 
+    # Add the voxel grid to the visualizer
+    vis.add_geometry(voxel_grid)
+
+    # run the visualizater
+    vis.run()
+    # Once the visualizer is closed destroy the window and clean up
+    vis.destroy_window()
+
+    fill_voxel_grid(voxel_grid, .25, 10)
+
+
+def fill_voxel_grid(v : o3d.geometry.VoxelGrid, voxel_size : int, bound : int):
     #make new pointcloud and then a new voxelGrid
     fullPoints = []
     fullColors = []
     xyz = []
-    for x in range(int(maxs[0]/voxel_size)):
-        for y in range(int(maxs[1]/voxel_size)):
-            for z in range(int(maxs[2]/voxel_size)):
+    for x in range(int(bound/voxel_size)):
+        for y in range(int(bound/voxel_size)):
+            for z in range(int(bound/voxel_size)):
                 xyz.append([x*voxel_size, y*voxel_size, z*voxel_size])
+
     
     #If the voxel exists in the one we created from the original point cloud, color it and add it to the new voxel grid
     #If the voxel does not exist in the original voxel grid, color it black and add it to the new voxel grid
     #The colors is how the AI will distinguish voxels that represent points and voxels that represent empty space
-    bools = voxel_grid.check_if_included(o3d.utility.Vector3dVector(xyz))
+    bools = v.check_if_included(o3d.utility.Vector3dVector(xyz))
     for i in range(len(bools)):
         if(bools[i]):
             fullPoints.append(xyz[i])
-            fullColors.append([.5, .5, .5])
+            fullColors.append([.5, .5, .5]) # could probably figure out how to get the correct colors into the full voxel grid
         else:
             fullPoints.append(xyz[i])
             fullColors.append([0, 0, 0])
@@ -43,7 +56,9 @@ def voxelize(points : list, voxel_size : int):
     fullpcd = o3d.geometry.PointCloud()
     fullpcd.points = o3d.utility.Vector3dVector(fullPoints)
     fullpcd.colors = o3d.utility.Vector3dVector(fullColors)
-    full_voxel_grid=o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(fullpcd, voxel_size, mins, maxs)
+    mins = np.array([0, 0, 0])      #min and max could be changed to function arguments for more manuverability
+    maxs = np.array([bound, bound, bound])
+    full_voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(fullpcd, voxel_size, mins, maxs)
 
     # for v in full_voxel_grid.get_voxels():
     #     print(v.grid_index)
